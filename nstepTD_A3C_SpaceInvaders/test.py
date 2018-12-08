8cc12ac67d2ca8bb4641032ea0e8e55918dfabb7
KrikunovEV <jen9wka@gmail.com>
2018-12-08 11:11:49 +0300

    up

--- nstepTD_A3C_SpaceInvaders/test.py
+++ nstepTD_A3C_SpaceInvaders/test.py
@@ -15,11 +15,17 @@ model3 = torch.nn.Sequential(
 
 tensor = torch.Tensor([1.0, 5.0])
 
+op = torch.optim.Adam(model.parameters())
+
 tensor = model(tensor)
 loss1 = model2(tensor).sum()
 loss2 = model3(tensor).sum()
 
-(loss1 + loss2).backward()
+op.zero_grad()
+Loss = loss1 + loss2
+Loss.backward()
+op.step()
+
 
 for param in model.parameters():
     print(param.grad)
